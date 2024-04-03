78e6e295abf168aad53a9877a70193ecfeb4f695
sky-inx <165698159+sky-inx@users.noreply.github.com>
2024-04-03 20:29:27 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,27 +7,9 @@ import Input from "@/widgets/Input";
 import Table from "@/widgets/Table";
 import { useState } from "react";
 
-export const getServerSideProps = async () => {
-  try {
-    const client = await clientPromise;
-    const db = client.db("ink");
-    const data = await db
-      .collection("water_based")
-      .find({})
-      .limit(20)
-      .toArray();
-    return {
-      props: { data: JSON.parse(JSON.stringify(data)) },
-    };
-  } catch (e) {
-    console.error(e);
-    return { props: { data: [] } };
-  }
-};
-
 export default function Home({ data }) {
   console.log("props: ", data);
-  const [search, setSearch] = useState("");
+  // const [search, setSearch] = useState("");
   const [open, setOpen] = useState(false);
   const handleCreate = async () => {
     await fetch("api/ink/water/create", {
