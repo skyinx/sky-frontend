05a0d9723cff3bb5924916ec2b28dbb2aa711427
Yash Kasetiya <sky.inx@protonmail.com>
2024-11-21 18:25:54 +0530

    Update Content Policy

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <Component {...pageProps} />
       <ToastContainer
         closeOnClick
